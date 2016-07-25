6db140aeaec8baba221061706923d1910d1c4787
Serhii Butenko <sereja.butenko@gmail.com>
2016-07-25 09:02:58 +0300

    Create project with a shared scheme for carthage

---
+++ EatFit/EatFit.h
@@ -0,0 +1,19 @@
+//
+//  EatFit.h
+//  EatFit
+//
+//  Created by Serhii Butenko on 25/7/16.
+//  Copyright © 2016 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EatFit.
+FOUNDATION_EXPORT double EatFitVersionNumber;
+
+//! Project version string for EatFit.
+FOUNDATION_EXPORT const unsigned char EatFitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EatFit/PublicHeader.h>
+
+
